79bbee6190458d420dd47b28990313bf690d4829
tomoki-ikarashi <lb.s0172@gmail.com>
2020-03-22 06:02:52 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :admin, controllers: {
-  sessions:      'admin/sessions',
-  passwords:     'admin/passwords',
-  registrations: 'admin/registrations'
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords',
+    registrations: 'admin/registrations'
   }
 	devise_for :customers, controllers: {
-	sessions:      'customers/sessions',
-	passwords:     'customers/passwords',
-	registrations: 'customers/registrations'
+  	sessions:      'customers/sessions',
+  	passwords:     'customers/passwords',
+  	registrations: 'customers/registrations'
 	}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root to: "homes#top"
