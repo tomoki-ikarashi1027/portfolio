81795de775825d06a0f8ccb1641650b6b65d74d5
tomoki-ikarashi <lb.s0172@gmail.com>
2020-03-25 09:37:45 +0000

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,25 +20,7 @@
 // require turbolinks
 
 
-function set2fig(num) {
-   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
-   var ret;
-   if( num < 10 ) { ret = "0" + num; }
-   else { ret = num; }
-   return ret;
-}
-function showClock2() {
 
-   var nowTime = new Date();
-   var nowHour = set2fig( nowTime.getHours() );
-   var nowMin  = set2fig( nowTime.getMinutes() );
-
-   var msg =  "時刻 " + nowHour + "時" + nowMin + "分" ;
-
-   document.getElementById("RealtimeClockArea2").innerHTML = msg;
-
-}
-setInterval('showClock2()',1000);
 
 
 $(function(){
