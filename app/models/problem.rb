851bc6ab26aee98bcc374b1a7b15bc4e51305a74
tomoki-ikarashi <lb.s0172@gmail.com>
2020-03-30 13:41:16 +0000

    add

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -3,4 +3,15 @@ class Problem < ApplicationRecord
 	belongs_to :category
 	has_many :comments, dependent: :destroy
 	validates :body, presence: true, length: { in: 10..300 }
+
+	def self.search(search,problem_name)
+	    if search
+	     	  Problem.where(['body LIKE ?', "%#{search}%"])
+	    elsif problem_name
+	   		  category_ids = Category.where("problem_name = ?", problem_name).pluck(:id)
+			  Problem.where("category_id IN (?)",category_ids )
+	    else
+	          Problem.all
+	    end
+	end
 end
