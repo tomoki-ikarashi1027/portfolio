b4490b22c996df199bf61069e335217855d9cf2c
tomoki-ikarashi <lb.s0172@gmail.com>
2020-05-15 05:00:46 +0000

    非言語処理修正-レスポンシブ修正

--- app/controllers/admins/comments_controller.rb
+++ app/controllers/admins/comments_controller.rb
@@ -1,11 +1,13 @@
 class Admins::CommentsController < ApplicationController
 	before_action :authenticate_admin!
 	def index
-		@comments = Comment.all
+		@comments = Comment.all.page(params[:page]).per(10).reverse_order
 	end
 	def destroy
 		@comment = Comment.find(params[:id])
+		@problem = @comment.problem
 		@comment.destroy
+		redirect_to admins_comments_path
 	end
 	private
 	def comment_params
